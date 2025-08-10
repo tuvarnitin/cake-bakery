fc37ae6da0a1e2df37a2609fdc1b6dd70adc9a10
Nitin <135065479+nitintuvar@users.noreply.github.com>
2025-08-10 22:38:22 +0530

    btn bg color

--- src/components/CartBtn.jsx
+++ src/components/CartBtn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CartBtn = () => {
   return (
-    <div className="shadow-lg max-h-40 overflow-hidden shadow-purple-800 py-2 px-3 gap-3 rounded-full flex flex-col items-center justify-end text-xl bg-purple-300 fixed right-20 bottom-10">
+    <div className="shadow-lg max-h-40 overflow-hidden shadow-red-800 py-2 px-3 gap-3 rounded-full flex flex-col items-center justify-end text-xl bg-purple-300 fixed right-20 bottom-10">
       <i className="ri-shopping-cart-line font-bold text-purple-900"></i>
     </div>
   );
